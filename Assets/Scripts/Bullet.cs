29bcff5f675a2f3440fc7054519a13cb4518327d
FieryBlade-313 <mohdfauz.s18@iiits.in>
2020-07-27 12:38:24 +0530

    Add basic game structure

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     
-    public float speed = 10f;
-
+    public float speed = 50f;
+    public Camera cam;
     private Vector2 AxisInput = new Vector2(0,0);
-    private Rigidbody rb;
+    private Vector2 mousePos = new Vector2(0,0);
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         AxisInput = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
         // print(AxisInput);
         // print(rb.velocity);
-        rb.velocity = new Vector3(AxisInput[0],AxisInput[1],0);
+        // print(shoot);
+        // print(transform.rotation);
+        Vector2 lookDir = mousePos -  rb.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.MovePosition(rb.position+AxisInput*speed*Time.fixedDeltaTime);
+        rb.rotation = angle;
     }
 }
