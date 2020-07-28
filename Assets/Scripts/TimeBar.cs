d1454bef59666e02bf0fed7bfcc68a3fed5f0084
FieryBlade-313 <mohdfauz.s18@iiits.in>
2020-07-28 17:10:50 +0530

    Add Slow motion Power Bar

---
+++ Assets/Scripts/TimeBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeBar : MonoBehaviour
+{
+    public Slider slider;
+    public void SetMaxTime(float maxTime)
+    {
+        slider.maxValue = maxTime;
+        slider.value = maxTime;
+    }
+    public void SetTimeVal(float time)
+    {
+        slider.value = time;
+    }
+}
--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -6,19 +6,47 @@ public class TimeController : MonoBehaviour
 {
     public TimeManager time_mgr;
 
+    public float maxTime = 5f;
+    private float currTime;
+
+    public float barDepleteMultiplier = 1f;
+    public float barFillMultiplier = 2.5f;
+
+    public TimeBar timeBar;
+
     [HideInInspector]
     public bool isSlow = false;
+
+    private void Start()
+    {
+        currTime = maxTime;
+        timeBar.SetMaxTime(maxTime);
+    }
+
     void Update()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(currTime > 0 && Input.GetButtonDown("Jump"))
         {
             time_mgr.SlowDown();
             isSlow = true;
         }
-        if(Input.GetButtonUp("Jump"))
+        if(currTime <= 0 || Input.GetButtonUp("Jump"))
         {
             time_mgr.NormalTime();
             isSlow = false;
         }
+    }
+    private void FixedUpdate()
+    {
+        if(isSlow)
+        {
+            currTime = currTime - Time.fixedUnscaledDeltaTime*barDepleteMultiplier;
+        }
+        else
+        {
+            currTime = currTime + Time.fixedUnscaledDeltaTime*barFillMultiplier;
+        }
+        currTime = Mathf.Clamp(currTime,0,maxTime);
+        timeBar.SetTimeVal(currTime);
     }
 }
